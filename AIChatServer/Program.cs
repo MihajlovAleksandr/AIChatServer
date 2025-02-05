f976839758b23f3b91355afb789743946206ec90
Oleg Petrovic <mihajlovaleksandr937@gmail.com>
2025-02-06 01:59:05 +0300

    Добавьте файлы проекта.

---
+++ AIChatServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://192.168.100.14:5000/");
+        listener.Start();
+        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            if (context.Request.IsWebSocketRequest)
+            {
+                HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                WebSocket webSocket = webSocketContext.WebSocket;
+
+                await ProcessWebSocketRequest(webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    static async Task ProcessWebSocketRequest(WebSocket webSocket)
+    {
+        byte[] buffer = new byte[1024 * 4];
+        WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!result.CloseStatus.HasValue)
+        {
+            string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine("Получено сообщение от клиента: " + message);
+
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+    }
+}
