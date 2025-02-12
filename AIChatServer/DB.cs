b6cc58f76d387478c00a2120d97e17d48ba81a92
Oleg Petrovic <mihajlovaleksandr937@gmail.com>
2025-02-12 11:06:38 +0300

    AddLoginIn

--- AIChatServer/DB.cs
+++ AIChatServer/DB.cs
@@ -1,4 +1,6 @@
 ﻿using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
 using MySql.Data.MySqlClient;
 namespace AIChatServer
 {
@@ -54,6 +56,79 @@ namespace AIChatServer
                     }
                 }
             }
+        }
+        private static User GetUserByUsername(string username)
+        {
+            string getUsersQuery = @"
+                SELECT u.Id, u.Username, u.Name, u.Age, u.Gender, preference,
+                p.MinAge, p.MaxAge, p.Gender as PrefGender
+                FROM Users u  
+                LEFT JOIN Preferences p ON u.Preference = p.Id WHERE u.Username = @username";
+            using (var connection = GetConnection())
+            {
+                using (var getUsersCommand = new MySqlCommand(getUsersQuery, connection))
+                {
+                    getUsersCommand.Parameters.AddWithValue("@username", username);
+
+                    using (var reader = getUsersCommand.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var user = new User
+                            {
+                                id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                username = reader.GetString(reader.GetOrdinal("Username")),
+                                name = reader.GetString(reader.GetOrdinal("Name")),
+                                age = reader.GetInt32(reader.GetOrdinal("Age")),
+                                gender = reader.GetChar(reader.GetOrdinal("Gender")),
+                                preference = new Preference
+                                {
+                                    Id = reader.IsDBNull(reader.GetOrdinal("Preference")) ? 0 : reader.GetInt32(reader.GetOrdinal("preference")),
+                                    MinAge = reader.IsDBNull(reader.GetOrdinal("MinAge")) ? 0 : reader.GetInt32(reader.GetOrdinal("MinAge")),
+                                    MaxAge = reader.IsDBNull(reader.GetOrdinal("MaxAge")) ? 0 : reader.GetInt32(reader.GetOrdinal("MaxAge")),
+                                    Gender = reader.IsDBNull(reader.GetOrdinal("PrefGender")) ? null : reader.GetChar("PrefGender")
+                                }
+                            };
+                            return user;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+        public static User LoginIn(string username, string password)
+        {
+            User user = GetUserByUsername(username);
+            if (user != null)
+            {
+                if (VerifyPassword(password, user.password))
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+        private static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string hashedInput = HashPassword(password);
+            return hashedInput == hashedPassword;
         }
 
 
--- AIChatServer/UnknownUser.cs
+++ AIChatServer/UnknownUser.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Org.BouncyCastle.Asn1;
 
 namespace AIChatServer
 {
     public class UnknownUser : ServerUser
     {
-        public event EventHandler<EventArgs> UserChanged;
+        public event EventHandler<User> UserChanged;
         public UnknownUser()
         {
             CommandGot += (command) =>
             {
                 if (command.Operation == "LoginIn")
                 {
+                    string username = command.GetData<string>("username");
+                    string password = command.GetData<string>("password");
                     
                 }
                 else if(command.Operation == "Registration")
